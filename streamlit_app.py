9709684b45958aaf044a8dc1d4f0862db589bca9
Leon Rosenkranz <Leon.rosenkranz@tu-dortmund.de>
2023-02-04 16:34:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Techlabs 1 first step: Uber pickups in NYC')
+st.title('Techlabs')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
