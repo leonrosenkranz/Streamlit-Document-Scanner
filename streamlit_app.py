595856f6c3980327c49ff37db865053667bac3d6
Leon Rosenkranz <Leon.rosenkranz@tu-dortmund.de>
2023-02-05 19:48:25 +0100

    Revert "Update streamlit_app.py"
    This reverts commit 84d7caaf13a16728e488fd36cde707a224589ef5.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,29 +6,15 @@ import io
 from PIL import Image
 
 
-def first_page():
-    st.write("Willkommen auf der ersten Seite")
-    if st.button("Zur zweiten Seite wechseln"):
-        second_page()
+st.set_page_config(page_title="Bild-Upload-App", page_icon=":camera:", layout="wide")
+st.title('Techlabs Document Scanner')
 
-    st.set_page_config(page_title="Bild-Upload-App", page_icon=":camera:", layout="wide")
-    st.title('Techlabs Document Scanner')
+# Eingabemaske zur Auswahl des Bildes
+file = st.file_uploader("Wähle ein Bild zum Hochladen aus", type=["jpg", "jpeg", "png"])
 
-    # Eingabemaske zur Auswahl des Bildes
-    file = st.file_uploader("Wähle ein Bild zum Hochladen aus", type=["jpg", "jpeg", "png"])
-
-    if file:
-        image = file.getvalue()
-        response = requests.post("http://localhost:5000/predict", data=image)
-        st.write("Das hochgeladene Bild:", response.content)
-
-def second_page():
-    st.write("Willkommen auf der zweiten Seite")
-    if st.button("Zurück zur ersten Seite"):
-        first_page()
-
-first_page()
-
-
+if file:
+    image = file.getvalue()
+    response = requests.post("http://localhost:5000/predict", data=image)
+    st.write("Das hochgeladene Bild:", response.content)
 
 
