06e379da28e517c0cb347c178d39ec33f34a1067
Leon Rosenkranz <Leon.rosenkranz@tu-dortmund.de>
2023-02-05 19:43:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,26 +18,3 @@ if file:
     st.write("Das hochgeladene Bild:", response.content)
 
 
-def upload_image():
-    image_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-
-    if image_file is not None:
-        image = Image.open(image_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-        return image
-    return None
-
-def remove_image():
-    st.warning("Image has been removed.")
-
-def main():
-    st.set_page_config(page_title="Image Uploader", page_icon=":camera:", layout="wide")
-    uploaded_image = upload_image()
-
-    if uploaded_image is not None:
-        st.sidebar.title("Actions")
-        if st.sidebar.button("Remove Image"):
-            remove_image()
-
-if __name__ == "__main__":
-    main()
