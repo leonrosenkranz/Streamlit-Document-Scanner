a2d8ac2ec4a1a608aee2c9f6a608a3366350f093
Leon Rosenkranz <Leon.rosenkranz@tu-dortmund.de>
2023-02-10 11:50:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,9 @@ from PIL import Image
 # Eingabemaske zur Auswahl des Bildes
 
 st.set_page_config(page_title="Bild-Upload-App", page_icon=":camera:", layout="wide")
-st.title('Techlabs Document Scanner')
+st.title('TechLabs Document Scanner')
+
+st.markdown("<br><hr style='border-top: 10px solid green'><br>", unsafe_allow_html=True)
 
 uploaded_file = st.file_uploader("Wählen Sie ein Bild aus", type=["jpg", "jpeg", "png"])
 
@@ -21,6 +23,8 @@ if uploaded_file is not None:
 
 # Credits
 
+st.markdown("<br><hr style='border-top: 10px solid green'><br>", unsafe_allow_html=True)
 st.markdown("An AI solution by TechLabs X <span style='color: green'>math</span>SCAN®", unsafe_allow_html=True)
+st.image("C:\Users\leon.rosenkranz\Documents\Studium\Techlabs\Git repository\Streamlit-Document-Scanner\Icons\TechLabs.png")
 
 
