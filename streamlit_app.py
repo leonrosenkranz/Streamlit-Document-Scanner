4a4576883af976a52d2d613c4dd294a15a5260e5
Leon Rosenkranz <Leon.rosenkranz@tu-dortmund.de>
2023-02-10 11:43:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ from PIL import Image
 
 st.set_page_config(page_title="Bild-Upload-App", page_icon=":camera:", layout="wide")
 st.title('Techlabs Document Scanner')
-st.markdown("TechLabs X <span style='color: green'>math</span>SCAN®", unsafe_allow_html=True)
 
 uploaded_file = st.file_uploader("Wählen Sie ein Bild aus", type=["jpg", "jpeg", "png"])
 
@@ -20,5 +19,8 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
+# Credits
+
+st.markdown("An AI solution by TechLabs X <span style='color: green'>math</span>SCAN®", unsafe_allow_html=True)
 
 
