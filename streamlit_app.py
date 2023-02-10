414e1cb1f72ac5d3923804d0e60e9c1cf5e24f9c
Leon Rosenkranz <Leon.rosenkranz@tu-dortmund.de>
2023-02-10 11:31:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,9 @@ if file:
     st.write("Das hochgeladene Bild:", response.content)
 
 
+uploaded_file = st.file_uploader("Wählen Sie ein Bild aus", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
