a2df908eb372756a69df4ac3835f31cc50f27dbd
Leon Rosenkranz <Leon.rosenkranz@tu-dortmund.de>
2023-02-10 13:07:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,27 +6,27 @@ import io
 from PIL import Image
 
 
-# Eingabemaske zur Auswahl des Bildes
+# Input interface for selection of the image
 
 st.set_page_config(page_title="Bild-Upload-App", page_icon=":camera:", layout="wide")
 st.title('TechLabs Document Scanner')
 
 st.markdown("<br><hr style='border-top: 10px solid green'><br>", unsafe_allow_html=True)
 
-uploaded_file = st.file_uploader("Wählen Sie ein Bild aus", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Select an image of mathematical expression", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
+st.markdown("<br><hr style='border-top: 10px solid green'><br>", unsafe_allow_html=True)
+
 
 # Credits
 
-st.markdown("<br><hr style='border-top: 10px solid green'><br>", unsafe_allow_html=True)
 image_url = "https://techlabs.org/static/tl-logo-white-b4f7f9cac2eabf0e15d37fc9be3db918.png"
 st.image(image_url, width=400)
 st.markdown("© 2023 - TechLabs X <span style='color: green'>math</span>SCAN®", unsafe_allow_html=True)
-
 st.markdown("<h2>Get in contact</h2><br>", unsafe_allow_html=True)
 
 linkedin_url_ayman = "https://de.linkedin.com/in/ayman-soultana-3408a5192"
