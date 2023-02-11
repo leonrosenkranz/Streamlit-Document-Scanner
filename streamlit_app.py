2095a6316149002d514131b1afc37d43896a1b15
Leon Rosenkranz <Leon.rosenkranz@tu-dortmund.de>
2023-02-11 17:24:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import numpy as np
 import requests
 import io
 from PIL import Image
+import tensorflow as tf
+import keras
 
 
 # Input interface for selection of the image
@@ -20,6 +22,35 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
 st.markdown("<br><hr style='border-top: 10px solid green'><br>", unsafe_allow_html=True)
+
+
++# Preprocessing
+img_height = 825
+img_width = 825
+
+def encode_single_sample(img):
+    #chane img_path correctly
+    #img_path = pathOfDirectory + img_path
+    # 1. Read image
+    #img = tf.io.read_file(img_path)
+    #2. Decode and convert to grayscale
+    img = tf.io.decode_png(img, channels=1)
+    # 3. Convert to float32 in [0, 1] range
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # 4. Resize to the desired size
+    img = tf.image.resize_with_pad(img, img_height, img_width)
+    # 5. Transpose the image because we want the time
+    # dimension to correspond to the width of the image.
+    img = tf.transpose(img, perm=[1, 0, 2])
+    # 4. Map the characters in label to numbers
+    #label = label
+    # 5. Return a dict as our model is expecting two inputs
+    #return {"image": img, "label": label}
+    return img
+
+model = keras.models.load_model("C:\Users\leon.rosenkranz\Documents\Studium\Techlabs\Git repository\Streamlit-Document-Scanner\Model\trainedModel\saved_model.pb")
+img = encode_single_sample(image)
+model.predict(img)
 
 
 # Credits
